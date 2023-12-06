04bbc8f9fbf164ad0becb832ac7512adf907f8b7
yed <redortech@gmail.com>
2023-12-06 14:27:19 +0800

    remove search

--- pages/winner.vue
+++ pages/winner.vue
@@ -2,14 +2,6 @@
   <v-card>
     <v-card-title>
       MLNG 45TH ANNIVERSARY | LIST OF LUCKY DRAW (5th December 2023)
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
     </v-card-title>
     <v-data-table
       :headers="headers"
