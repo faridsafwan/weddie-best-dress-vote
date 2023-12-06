cdc65111c425ec3441dfcdcee4dd42f6200ba345
yed <redortech@gmail.com>
2023-12-06 13:11:12 +0800

    update

---
+++ pages/button-v2.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <button @click="handleButtonClick" class="animated-button">
+      Click me
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleButtonClick() {
+      // Your button click logic here
+      console.log('Button clicked!');
+    },
+  },
+};
+</script>
+
+<style>
+.animated-button {
+  /* Add your button styles here */
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.animated-button:active {
+  /* Add your animation styles here */
+  background-color: #3498db; /* Change to the color you want during the click animation */
+  animation: clickAnimation 0.3s ease;
+}
+
+@keyframes clickAnimation {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
---
+++ pages/winner.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card>
+    <v-card-title>
+      MLNG 45TH ANNIVERSARY | LIST OF LUCKY DRAW (5th December 2023)
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="search"
+      hide-default-footer
+      disable-pagination
+      loading="true"
+      dense
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        { text: "Name", value: "name" },
+        { text: "Number", value: "number" },
+        { text: "Prize", value: "prize" },
+      ],
+      items: [],
+      search: "",
+    };
+  },
+
+  async mounted() {
+    await this.fetchWinner();
+  },
+
+  methods: {
+    async fetchWinner() {
+      try {
+        const response = await this.$axios.get("/winners");
+        this.items = response.data;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+/* Add your custom styles here */
+</style>
